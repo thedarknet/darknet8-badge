385dd68995251820d60de3218711b2c04607865d
cmdc0de <cmdc0dez@gmail.com>
2019-07-07 07:21:16 +0000

    almost ready to start menuing logic

--- DC27/firmware/main/component.mk
+++ DC27/firmware/main/component.mk
@@ -5,6 +5,6 @@
 
 
 #Compile image file into the resulting firmware binary
-COMPONENT_EMBED_FILES := image.jpg
+#COMPONENT_EMBED_FILES := image.jpg
 
 COMPONENT_EMBED_TXTFILES := $(PROJECT_PATH)/server_certs/ca_cert.pem
