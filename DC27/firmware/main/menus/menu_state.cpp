f06cb3042da42f787e683bdf732568d824bb0251
Gregory Price <gregory@gregoryprice.net>
2019-07-27 01:19:12 -0400

    update menu_state.cpp with PairingMenu and add OTA Menu

--- DC27/firmware/main/menus/menu_state.cpp
+++ DC27/firmware/main/menus/menu_state.cpp
@@ -62,6 +62,8 @@ ErrorType MenuState::onInit() {
 	Items[7].text = (const char *) "Test Badge";
 	Items[8].id = 8;
 	Items[8].text = (const char *) "Calibrate Touch";
+	Items[9].id = 9;
+	Items[9].text = (const char *) "Badge Update";
 	DN8App::get().getDisplay().fillScreen(RGBColor::BLACK);
 	DN8App::get().getGUI().drawList(&this->MenuList);
 	//empty queue
@@ -105,7 +107,7 @@ libesp::BaseMenu::ReturnStateContext MenuState::onRun() {
 					break;
 				case 1:
 					if (DN8App::get().getContacts().getSettings().getAgentName()[0] != '\0') {
-						//nextState = DN8App::get().getPairingState();
+						nextState = DN8App::get().getPairingMenu();
 					} else {
 						nextState = DN8App::get().getDisplayMessageState(DN8App::get().getMenuState(),
 								(const char *) "You must set your agent name first", 3000);
@@ -135,9 +137,6 @@ libesp::BaseMenu::ReturnStateContext MenuState::onRun() {
 					break;
 				case 9:
 					nextState = DN8App::get().getOTAMenu();
-					break;
-				case 9:
-					nextState = DN8App::get().getPairingMenu();
 					break;
 			}
 		}
