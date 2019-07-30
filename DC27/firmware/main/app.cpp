cfd4a1c286c76444498e444655550760679348c0
Gregory Price <gregory@gregoryprice.net>
2019-07-30 00:28:19 -0400

    remove the wifi stuff until i can fix it appropriately

--- DC27/firmware/main/app.cpp
+++ DC27/firmware/main/app.cpp
@@ -15,7 +15,7 @@
 #include <device/touch/XPT2046.h>
 #include "./ble.h"
 #include "./game_master.h"
-#include "./wifi.h"
+//#include "./wifi.h"
 #include "menus/menu_state.h"
 #include "buttons.h"
 #include "menus/calibration_menu.h"
@@ -76,7 +76,7 @@ BluetoothTask BTTask("BluetoothTask");
 GameTask GMTask("GameTask");
 ButtonInfo MyButtons;
 CalibrationMenu DN8CalibrationMenu;
-WIFITask WifiTask("WifiTask");
+//WIFITask WifiTask("WifiTask");
 
 const char *DN8ErrorMap::toString(int32_t err) {
 	return "TODO";
@@ -164,10 +164,11 @@ libesp::ErrorType DN8App::onInit() {
 		if(!GMTask.init()) {
 			return ErrorType(GAME_TASK_INIT_FAIL);
 		}
+/*
 		if(!WifiTask.init()) {
 			return ErrorType(WIFI_TASK_INIT_FAIL);
 		}
-
+*/
 		if(!MyButtons.init()) {
 			return ErrorType(BUTTON_INIT_FAIL);
 		} else {
@@ -181,7 +182,7 @@ libesp::ErrorType DN8App::onInit() {
 	BTTask.start();
 	GMTask.start();
 	BTTask.setGameTaskQueue(GMTask.getQueueHandle());
-	WifiTask.start();
+	//WifiTask.start();
 	
 	setCurrentMenu(getMenuState());
 	return et;
@@ -195,9 +196,11 @@ GameTask &DN8App::getGameTask() {
 	return GMTask;
 }
 
+/*
 WIFITask &DN8App::getWifiTask() {
 	return WifiTask;
 }
+*/
 
 uint16_t DN8App::getCanvasWidth() {
 	return FrameBuf.getBufferWidth(); 
