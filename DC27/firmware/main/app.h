6ebe414700b00c4f6979fc5d9852b078962b9608
Gregory Price <gregory@gregoryprice.net>
2019-07-30 04:22:43 -0400

    integrate SPIRAM into memory map model, increase resolution to 75%

--- DC27/firmware/main/app.h
+++ DC27/firmware/main/app.h
@@ -60,8 +60,8 @@ public:
 	static const char *sNO;
 	static const uint16_t DISPLAY_HEIGHT		= 240;
 	static const uint16_t DISPLAY_WIDTH			= 320;
-	static const uint16_t FRAME_BUFFER_HEIGHT	= 144;
-	static const uint16_t FRAME_BUFFER_WIDTH	= 192;
+	static const uint16_t FRAME_BUFFER_HEIGHT	= 180;
+	static const uint16_t FRAME_BUFFER_WIDTH	= 240;
 	static DN8App &get();
 public:
 	virtual ~DN8App();
